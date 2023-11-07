e9f405ec47f878324c080966f4be67c0544c4a05
LiliannyMarinho <liliannymarinho@gmail.com>
2023-11-07 15:16:56 -0300

    Update.2

--- Contato.java
+++ Contato.java
@@ -1,3 +1,27 @@
 public class Contato {
-    
+    String name;
+    String phoneNumber;
+
+    public Contato(String name, String phoneNumber){
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getPhoneNumber(){
+        return phoneNumber;
+    }
+    public void setPhoneNumber(String phoneNumber){
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public String toString(){
+        return "Contato: \nNome: "+name+"\nTelefone: "+phoneNumber;
+    }
 }
