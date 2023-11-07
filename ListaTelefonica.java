eab23a3942b38c1a1eb3d2f5fa447ee3289b1828
LiliannyMarinho <liliannymarinho@gmail.com>
2023-11-07 14:57:42 -0300

    Update.1

--- ListaTelefonica.java
+++ ListaTelefonica.java
@@ -1,3 +1,24 @@
 public class ListaTelefonica {
-    
+    private TabelaHashListaTelefonica gride_Contatos;
+
+    public ListaTelefonica(int sizeGride){
+        gride_Contatos = new TabelaHashListaTelefonica(sizeGride);
+    }
+
+    public void addContato(Contato contato){
+        gride_Contatos.insert(contato);
+    }
+
+    public void removContato(String name) throws Exception {
+        Contato contato = gride_Contatos.search(name);
+        gride_Contatos.remove(contato);
+    }
+
+    public Contato buscContato(String name) throws Exception {
+        return gride_Contatos.search(name);
+    }
+
+    public String printList(){
+        return gride_Contatos.print();
+    }
 }
